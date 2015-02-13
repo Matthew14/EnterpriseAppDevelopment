c059af65683d32f8741795bb04c94252a5afcd80
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-13 23:20:31 +0000

    more week 4

---
+++ Week4/app/DB/DAO/UsersDAO.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Luca + Matthew
+ * definition of the User DAO (database access object)
+ */
+class UsersDAO {
+	private $dbManager;
+	
+    function UsersDAO($DBMngr) {
+		$this->dbManager = $DBMngr;
+	}
+	function getUsers() {
+		$sql = "SELECT * ";
+		$sql .= "FROM users ";
+		$sql .= "ORDER BY users.name; ";
+
+		$result = $this->dbManager->executeQuery ( $sql );
+		$arrayOfResults = $this->dbManager->fetchResults ( $result );
+		return $arrayOfResults;
+	}
+
+	function insertUser($p) {
+        $sql = "INSERT INTO users (name, surname, email, password) VALUES ($p[name], $p[surname], $p[email], $p[password])";
+        $this->dbManager->excecuteQuery($sql);        
+	}
+}
+?>
---
+++ Week4/app/index.php
@@ -0,0 +1,65 @@
+<?php
+//create HTML form string
+
+
+//include DB manager and UsersDAO
+require_once('../slim/Slim/Slim.php');
+
+require_once('DB/DAO/UsersDAO.php');
+require_once('DB/DBManager.php');
+
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim(array('debug' => true));
+
+
+$app->get('/users/get', function () use ($app) {
+      $db = new DBManager();
+      $db->openConnection();
+
+      $udao = new UsersDAO($db);
+
+      $u = $udao->getUsers();
+
+      $response = $app->response();
+      $response['Content-Type'] = 'application/json';
+      $response->body(json_encode($u));
+
+});
+
+$app->get('/users/insert', function () use ($app){
+     $db = new DBManager();    
+     $db->openConnection();    
+     $udao = new UsersDAO($db);
+        
+     $inForm = "<html><form id='inUsers'>
+        Name: <input type='text' name='name'/>
+        Surname: <input type='text' name='surname' />
+        Password: <input type='password' name='password' />
+        Email: <input type='text' name='email' />
+        Doit: <input type='submit' value='submit' /></form>     
+     </html>";
+     echo $inForm;
+});
+
+
+$app->get('/info', function() {
+    echo phpinfo();
+});
+
+$app->run();
+
+
+
+//execute the 4 steps for retrieving the list of users(as per lecture notes)
+
+
+//generate the html list of users (<ul><li>user 1</li>...<li>user n</li></ul>) by looping through DB result set
+
+
+//echo both the form and the list of users
+
+?>
+  
+
