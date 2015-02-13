34610ac470540bcbaec1b37d06c4fb1c266a6189
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-02-13 23:17:54 +0000

    week 4

---
+++ Week4/app/index.php
@@ -0,0 +1,22 @@
+<?php
+//create HTML form string
+
+
+//include DB manager and UsersDAO
+require_once('DB/DAO/UsersDAO.php');
+require_once('DB/DAO/DBManager.php');
+
+
+//execute the 4 steps for retrieving the list of users(as per lecture notes)
+
+
+//generate the html list of users (<ul><li>user 1</li>...<li>user n</li></ul>) by looping through DB result set
+
+
+//echo both the form and the list of users
+
+?>
+<form id='adduserform'>
+
+
+</form>
---
+++ Week4/app/insertNewUser.php
@@ -0,0 +1,8 @@
+<?php
+// read $_GET associative array to retrieve information (all the parameters are compulsory, so check them)
+
+//include DB manager and UsersDAO
+
+//execute the 4 steps for inserting a new users (as per lecture notes)
+
+?>
