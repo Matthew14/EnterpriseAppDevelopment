b842f5e876900113a3ee18c67e3cafd79caf9250
Matthew O'Neill <14matthewoneill14@gmail.com>
2015-05-03 17:42:52 +0100

    remove slim from repo

--- Assignment/app/index.php
+++ Assignment/app/index.php
@@ -12,47 +12,47 @@ require_once "conf/config.inc.php";
 
 $app->map ( "/users(/:id)", function ($userID = null) use($app) {
 
-	$httpMethod = $app->request->getMethod();
-	if (($userID == null) or is_numeric ( $userID )) {
-		$parameters["id"] = $userID;		//prepare parameters to be passed to the controller (example: ID)
-		switch ($httpMethod) {
-			case "GET" :
-				if ($userID != null)
-					$action = ACTION_GET_USER;
-				else
-					$action = ACTION_GET_USERS;
-				break;
-			case "POST" :
-				$action = ACTION_CREATE_USER;
-				break;
-			case "PUT" :
-				$action = ACTION_UPDATE_USER;
-				break;
-			case "DELETE" :
-				$action = ACTION_DELETE_USER;
-				break;
-			default :
-		}
-	}
+    $httpMethod = $app->request->getMethod();
+    if (($userID == null) or is_numeric ( $userID )) {
+        $parameters["id"] = $userID;        //prepare parameters to be passed to the controller (example: ID)
+        switch ($httpMethod) {
+            case "GET" :
+                if ($userID != null)
+                    $action = ACTION_GET_USER;
+                else
+                    $action = ACTION_GET_USERS;
+                break;
+            case "POST" :
+                $action = ACTION_CREATE_USER;
+                break;
+            case "PUT" :
+                $action = ACTION_UPDATE_USER;
+                break;
+            case "DELETE" :
+                $action = ACTION_DELETE_USER;
+                break;
+            default :
+        }
+    }
 
-	return new loadRunMVCComponents ( "UserModel", "UserController", "jsonView", $action, $app, $parameters );
+    return new loadRunMVCComponents ( "UserModel", "UserController", "jsonView", $action, $app, $parameters );
 
 } )->via ( "GET", "POST", "PUT", "DELETE" );
 
 $app->run ();
 
 class loadRunMVCComponents {
-	public $model, $controller, $view;
-	public function __construct($modelName, $controllerName, $viewName, $action, $app, $parameters = null) {
-		include_once "models/" . $modelName . ".php";
-		include_once "controllers/" . $controllerName . ".php";
-		include_once "views/" . $viewName . ".php";
+    public $model, $controller, $view;
+    public function __construct($modelName, $controllerName, $viewName, $action, $app, $parameters = null) {
+        include_once "models/" . $modelName . ".php";
+        include_once "controllers/" . $controllerName . ".php";
+        include_once "views/" . $viewName . ".php";
 
-		$this->model = new $modelName (); // common model
-		$this->controller = new $controllerName ( $this->model, $action, $app, $parameters );
-		$this->view = new $viewName ( $this->controller, $this->model, $app ); // common view
-		$this->view->output (); // this returns the response to the requesting client
-	}
+        $this->model = new $modelName (); // common model
+        $this->controller = new $controllerName ( $this->model, $action, $app, $parameters );
+        $this->view = new $viewName ( $this->controller, $this->model, $app ); // common view
+        $this->view->output (); // this returns the response to the requesting client
+    }
 }
 
 ?>
